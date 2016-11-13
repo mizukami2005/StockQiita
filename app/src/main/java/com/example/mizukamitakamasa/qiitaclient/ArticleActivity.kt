bdc182852d85100d39f6d09b3443986b09e1e2c9
Takamasa Mizukami <mizukami2005@gmail.com>
2016-11-13 19:00:55 +0900

    :heavy_plus_sign: stock出来るように処理追加

--- app/src/main/java/com/example/mizukamitakamasa/qiitaclient/ArticleActivity.kt
+++ app/src/main/java/com/example/mizukamitakamasa/qiitaclient/ArticleActivity.kt
@@ -2,13 +2,24 @@ package com.example.mizukamitakamasa.qiitaclient
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
+import android.support.design.widget.FloatingActionButton
 import android.webkit.WebView
+import com.example.mizukamitakamasa.qiitaclient.client.ArticleClient
 import com.example.mizukamitakamasa.qiitaclient.model.Article
 import com.example.mizukamitakamasa.qiitaclient.view.ArticleView
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import javax.inject.Inject
 
 class ArticleActivity : AppCompatActivity() {
+
+  @Inject
+  lateinit var articleClient: ArticleClient
 
   companion object {
 
@@ -21,13 +32,35 @@ class ArticleActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    (application as QiitaClientApp).component.inject(this)
     setContentView(R.layout.activity_article)
 
+//    val collapsongToolBarLayout = findViewById(R.id.collapsing_toolbar) as CollapsingToolbarLayout
+    val stockButton = findViewById(R.id.stock_button) as FloatingActionButton
     val articleView = findViewById(R.id.article_view) as ArticleView
     val webView = findViewById(R.id.web_view) as WebView
 
     val article: Article = intent.getParcelableExtra(ARTICLE_EXTRA)
     articleView.setArticle(article)
     webView.loadUrl(article.url)
+
+    stockButton.setOnClickListener {
+      val data = getSharedPreferences("DataToken", Context.MODE_PRIVATE)
+      val token = data.getString("token", "")
+      if (token.length != 0) {
+        articleClient.stock("Bearer $token", article.id)
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
+          .doAfterTerminate {
+            //todo ボタンの色を変える
+          }
+          .bindToLifecycle(MainActivity())
+          .subscribe({
+            toast("stock: $it")
+          }, {
+            toast("stock出来ませんでした: $it")
+          })
+      }
+    }
   }
 }
