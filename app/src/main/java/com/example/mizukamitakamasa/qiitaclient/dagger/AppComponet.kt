90a1427039f94443da3a971d6398730499214a7e
Takamasa Mizukami <mizukami2005@gmail.com>
2016-11-23 21:48:40 +0900

    :heavy_plus_sign: inject追加

--- app/src/main/java/com/example/mizukamitakamasa/qiitaclient/dagger/AppComponet.kt
+++ app/src/main/java/com/example/mizukamitakamasa/qiitaclient/dagger/AppComponet.kt
@@ -1,6 +1,7 @@
 package com.example.mizukamitakamasa.qiitaclient.dagger
 
 import com.example.mizukamitakamasa.qiitaclient.ArticleActivity
+import com.example.mizukamitakamasa.qiitaclient.ListTagActivity
 import com.example.mizukamitakamasa.qiitaclient.MainActivity
 import com.example.mizukamitakamasa.qiitaclient.fragment.ViewPageListFragment
 import dagger.Component
@@ -17,4 +18,5 @@ interface AppComponet {
   fun inject(mainActivity: MainActivity)
   fun inject(viewPageListFragment: ViewPageListFragment)
   fun inject(articleActivity: ArticleActivity)
+  fun inject(listTagActivity: ListTagActivity)
 }
