34d8c31009473ebc4994a6e47afb2f781ac81bfa
Takamasa Mizukami <mizukami2005@gmail.com>
2016-12-13 22:31:01 +0900

    :heavy_minus_sign: 不要になったコードを削除

--- app/src/main/java/com/example/mizukamitakamasa/qiitaclient/extensions.kt
+++ app/src/main/java/com/example/mizukamitakamasa/qiitaclient/extensions.kt
@@ -9,10 +9,6 @@ import android.widget.Toast
  * Created by mizukamitakamasa on 2016/09/25.
  */
 
-fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
-  findViewById(id) as T
-}
-
 fun Context.toast(message: String, duration: Int = Toast.LENGTH_LONG) {
   Toast.makeText(this, message, duration).show()
 }
