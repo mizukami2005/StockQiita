466c0be2571a3d6c9c8038430ad13788540bd96d
Takamasa Mizukami <mizukami2005@gmail.com>
2016-12-13 22:33:48 +0900

    :recycle: インデント修正

--- app/src/main/java/com/example/mizukamitakamasa/qiitaclient/model/ResponseToken.kt
+++ app/src/main/java/com/example/mizukamitakamasa/qiitaclient/model/ResponseToken.kt
@@ -8,22 +8,21 @@ import android.os.Parcelable
  */
 data class ResponseToken(val token: String) : Parcelable {
 
-    companion object {
-        @JvmField
-        val CRETOR: Parcelable.Creator<ResponseToken> = object : Parcelable.Creator<ResponseToken> {
-            override fun createFromParcel(source: Parcel): ResponseToken = source.run {
-                ResponseToken(readString())
-            }
-
-            override fun newArray(size: Int): Array<ResponseToken?> = arrayOfNulls(size)
-        }
+  companion object {
+    @JvmField
+    val CRETOR: Parcelable.Creator<ResponseToken> = object : Parcelable.Creator<ResponseToken> {
+      override fun createFromParcel(source: Parcel): ResponseToken = source.run {
+        ResponseToken(readString())
+      }
+      override fun newArray(size: Int): Array<ResponseToken?> = arrayOfNulls(size)
     }
+  }
 
-    override fun describeContents(): Int = 0
+  override fun describeContents(): Int = 0
 
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.run {
-            writeString(token)
-        }
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.run {
+      writeString(token)
     }
+  }
 }
