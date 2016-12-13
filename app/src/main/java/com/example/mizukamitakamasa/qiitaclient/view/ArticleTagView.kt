0a9b933eb3a55f9d8d2965c8b12ffadacc7d55c9
Takamasa Mizukami <mizukami2005@gmail.com>
2016-12-13 22:34:11 +0900

    :pencil2: ArticleTagViewにextension適用

--- app/src/main/java/com/example/mizukamitakamasa/qiitaclient/view/ArticleTagView.kt
+++ app/src/main/java/com/example/mizukamitakamasa/qiitaclient/view/ArticleTagView.kt
@@ -2,17 +2,12 @@ package com.example.mizukamitakamasa.qiitaclient.view
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
-import android.widget.CheckBox
 import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.mizukamitakamasa.qiitaclient.R
-import com.example.mizukamitakamasa.qiitaclient.bindView
 import com.example.mizukamitakamasa.qiitaclient.model.ArticleTag
-import com.example.mizukamitakamasa.qiitaclient.util.TagUtils
+import kotlinx.android.synthetic.main.view_article_tag.view.*
 
 /**
  * Created by mizukamitakamasa on 2016/11/19.
@@ -33,21 +28,13 @@ class ArticleTagView: FrameLayout {
               defStyleAttr: Int,
               defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
-  val tagImageView: ImageView by bindView(R.id.tag_image_view)
-
-  val tagNameTextView: TextView by bindView(R.id.tag_name_text_view)
-
-  val followersCountTextView: TextView by bindView(R.id.followers_count_text_view)
-
-  val tagCheckBox: CheckBox by bindView(R.id.tag_check_box)
-
   init {
     LayoutInflater.from(context).inflate(R.layout.view_article_tag, this)
   }
 
   fun setArticleTag(articleTag: ArticleTag) {
-    tagNameTextView.text = articleTag.id
-    followersCountTextView.text = articleTag.followers_count.toString() + " Followers"
-    Glide.with(context).load(articleTag.icon_url).into(tagImageView)
+    tag_name_text_view.text = articleTag.id
+    followers_count_text_view.text = articleTag.followers_count.toString() + " Followers"
+    Glide.with(context).load(articleTag.icon_url).into(tag_image_view)
   }
 }
